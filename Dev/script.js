1040b819423eeb4356f8733a7974ac44c4d006e9
Aaron <aaronthesnowboarder@live.com>
2021-02-22 20:57:57 -0800

    test3

--- Dev/script.js
+++ Dev/script.js
@@ -1,8 +1,38 @@
-var startButton =document.getElementById("Start Button");
+var startButton =document.getElementById("start");
 var gameDiv =document.getElementById("game")
 var timer =document.getElementById("timer")
 var words = document.getElementById('words')
 var reset = document.getElementById("reset")
+var game=[“boolen”, “Element” , “Doument”, “Game” ,“miniproject”];
+var choice1 =[math.random()*5];
+console.log(choice1)
+var choice= [math.floor(choice1)]
+console.log(choice)
+var answer = var game[choice]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
