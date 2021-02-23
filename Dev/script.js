c8abdd51081b9ad7504f780c34cc73662ec48b3e
Aaron <aaronthesnowboarder@live.com>
2021-02-22 20:32:19 -0800

    test2

---
+++ Dev/script.js
@@ -0,0 +1,8 @@
+var startButton =document.getElementById("Start Button");
+var gameDiv =document.getElementById("game")
+var timer =document.getElementById("timer")
+var words = document.getElementById('words')
+var reset = document.getElementById("reset")
+
+
+
